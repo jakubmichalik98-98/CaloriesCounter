1073bc6bfd99bbe15f6892db7ce111239e5a1a1a
Jakub <jakubmichalik98-98@wp.pl>
2020-03-13 16:45:08 +0100

    basic reg and log

--- CaloriesCounter/CaloriesCounter/urls.py
+++ CaloriesCounter/CaloriesCounter/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from users import views as user_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register')
-
+    path('register/', user_views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- CaloriesCounter/users/views.py
+++ CaloriesCounter/users/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 
 def register(request):
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Account created for {username}")
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
