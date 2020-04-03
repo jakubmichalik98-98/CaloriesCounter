631eeba7435523dffee99b7f7bc05f2973447a84
Jakub <jakubmichalik98-98@wp.pl>
2020-04-03 17:31:03 +0200

    reduce update

--- CaloriesCounter/diets/views.py
+++ CaloriesCounter/diets/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .forms import PersonalForm, AdvancedMealForm
+from .forms import PersonalForm, AdvancedMealForm, ReduceForm
 from django.http import HttpResponseRedirect
-from .models import AdvancedMeal
-from .services import AdvancedModelDataService, PersonalFormDataService
+from .models import AdvancedMeal, ReduceKcal
+from .services import AdvancedModelDataService, PersonalFormDataService, CountingReducedCaloriesService, \
+    SummaryCaloriesService
 
 
 def get_personal_form(request):
@@ -40,16 +41,37 @@ def get_meal_form(request):
     return render(request, 'diets/meal.html', {'meal_form': meal_form})
 
 
+def reduce_form(request):
+    if request.method == "POST":
+        reduced_form = ReduceForm(request.POST)
+        if reduced_form.is_valid():
+            activity = reduced_form.cleaned_data["activity"]
+            hours = reduced_form.cleaned_data["hours"]
+            reduce_kcal = ReduceKcal(activity=activity, hours=hours)
+            reduce_kcal.save()
+            return HttpResponseRedirect("/diets/advanced/")
+
+    else:
+        reduced_form = ReduceForm()
+
+    return render(request, "diets/reduced.html", {"reduced_form": reduced_form})
+
+
 def advanced_info(request):
     todays_objects = AdvancedMeal.today_objects.all()
+    todays_reduce = ReduceKcal.today_objects.all()
+    counting_reduce = CountingReducedCaloriesService(todays_reduce)
     service_meal = AdvancedModelDataService(todays_objects)
     eaten_kcal_sum = service_meal.sum_of_kcal()
     eaten_proteins_sum = service_meal.sum_of_proteins()
     eaten_fats_sum = service_meal.sum_of_fats()
     eaten_carbohydrates_sum = service_meal.sum_of_carbohydrates()
+    reduced_calories = counting_reduce.get_reduced_kcal()
 
     data_dict = request.session["data_dict"]
     service_personal_form = PersonalFormDataService(data_dict)
+
+    summary_of_kcal = SummaryCaloriesService(eaten_kcal_sum, counting_reduce.get_reduced_kcal()).get_summary_of_kcal()
 
     ppm = service_personal_form.get_ppm()
     required_proteins = service_personal_form.required_proteins()
@@ -60,6 +82,6 @@ def advanced_info(request):
                'carbohydrates_sum': eaten_carbohydrates_sum,
                'fats_sum': eaten_fats_sum, "required_fats": required_fats,
                "required_carbohydrates": required_carbohydrates,
-               "required_proteins": required_proteins}
+               "required_proteins": required_proteins, "reduced_calories": reduced_calories, "summary": summary_of_kcal}
 
     return render(request, "diets/advanced.html", context)
